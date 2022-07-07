d1731a70ff9d153bef0f5bf42b07128ca64bb8c6
sam's macbook <samuel2.martinez2@gmail.com>
2022-07-07 06:21:31 -0500

    select exercise done

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,10 @@ DROP TABLE IF EXISTS albums;
 USE codeup_test_db;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_first_last_name VARCHAR(100),
+    artist VARCHAR(100),
+    name VARCHAR (100),
     release_date INT UNSIGNED NOT NULL,
-    sales DECIMAL (6.2) NOT NULL,
+    sales DECIMAL (6,2) NOT NULL,
     genre VARCHAR(50),
     PRIMARY KEY (id)
 );
---
+++ select_exercises.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+SELECT '---Pink Floyd album names---' AS 'Info';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT '---Sgt. Pepper"s Lonely Hearts Club Band release date---' AS 'Info';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper"s Lonely Hearts Club Band';
+
+SELECT '---genre from Nevermind---' AS 'Info';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT '---albums released in the 90s---' AS 'Info';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT '---albums with less then 20 mil sales---' AS 'Info';
+SELECT name FROM albums WHERE sales < '30';
+
+SELECT '---ROCK ALBUMS---' AS 'Info';
+SELECT name FROM albums WHERE genre = 'Rock';
